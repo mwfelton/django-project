7bada04f2d2e3ba0ad30f3ab113f1425f8bb4930
Miles Felton Gerber <mwfelton03@gmail.com>
2025-01-11 15:06:02 +0000

    Forms and collecting data

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,10 +1,17 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LandingPageForm
 
 def home_page(request, *args, **kwargs):
-    title = "Welcome home"
+    title = "Welcome Burger"
+    form = LandingPageForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = LandingPageForm()
+        
     context = {
-        "title": title
+        "title": title,
+        "form": form
     }
     # parag = "{title} Justin!".format(**context)
     return render(request, "home.html", context)
