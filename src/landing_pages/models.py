23a755d803be7379ada939530827afc009980245
Miles Felton Gerber <mwfelton03@gmail.com>
2025-01-31 16:03:05 +0000

    admin panel tweaks

--- src/landing_pages/models.py
+++ src/landing_pages/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class LandingPageEntry(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=220)
     email = models.EmailField()
+
+def __str__(self):
+    return f"{self.email}"
